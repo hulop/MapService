b07231d9fa5e10f95476bfc09cd0ef943b3aea26
Takashi Itoh <jl03313@jp.ibm.com>
2018-07-14 01:05:35 +0900

    Fix download script

--- download-lib.sh
+++ download-lib.sh
@@ -2,14 +2,15 @@ pwd=`pwd`
 
 cd ./MapService/WebContent
 curl -L -O https://github.com/openlayers/openlayers/releases/download/v4.6.4/v4.6.4-dist.zip
-unzip v4.6.4-dist.zip && rm v4.6.4-dist.zip
+unzip -o v4.6.4-dist.zip && rm v4.6.4-dist.zip
+rm -rf openlayers/v4.6.4
 mv v4.6.4-dist openlayers/v4.6.4
 
 cd jquery
 curl -L -O https://code.jquery.com/jquery-1.11.3.min.js
 curl -L -O https://rawgit.com/arschmitz/jquery-mobile-nestedlists/master/jquery.mobile.nestedlists.js
 curl -L -O https://jquerymobile.com/resources/download/jquery.mobile-1.4.5.zip
-unzip jquery.mobile-1.4.5.zip -x "demos/**" "jquery.mobile.*" && rm jquery.mobile-1.4.5.zip
+unzip -o jquery.mobile-1.4.5.zip -x "demos/**" "jquery.mobile.*" && rm jquery.mobile-1.4.5.zip
 
 cd $pwd
 
