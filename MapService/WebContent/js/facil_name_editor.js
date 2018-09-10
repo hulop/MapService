9b9e937df70501b0324bc7972f28153967483f95
Takashi Itoh <jl03313@jp.ibm.com>
2018-09-10 14:36:44 +0900

    Support editing on facility names table

--- MapService/WebContent/js/facil_name_editor.js
+++ MapService/WebContent/js/facil_name_editor.js
@@ -1,4 +1,5 @@
 var $names = [ '施設ID', 'floors', 'building', 'major_category', 'sub_category', 'show_labels_zoomlevel' ];
+var $numbers = [];
 $names = $names.concat([ '名称', '名称:ja', '名称:ja-Pron', '名称:en', '名称:es', '名称:fr', '名称:ko', '名称:zh-CN' ]);
 $names = $names.concat([ 'long_description', 'long_description:ja', 'long_description:ja-Pron', 'long_description:en',
 		'long_description:es', 'long_description:fr', 'long_description:ko', 'long_description:zh-CN' ]);
@@ -9,6 +10,44 @@ $(document).ready(function() {
 	var $hulop = window.opener.$hulop;
 	function floorText(floor) {
 		return (floor < 0 ? 'B' + (-floor) : floor) + 'F';
+	}
+	function createCell(feature, name, value, editable, rowspan) {
+		var color = '#eee';
+		if (editable) {
+			color = '#fff';
+			if (value) {
+				var text = value.trim().replace(/[\t\r\n]/g, ' ');
+				if (value != text) {
+					console.error('"' + value + '" > "' + text + '"')
+					feature.set(name, value = text);
+					color = 'lightblue';
+				}
+			}
+		}
+		var td = $('<td>', {
+			'rowspan' : rowspan || 1,
+			'contenteditable' : editable,
+			'css' : {
+				'background-color' : color
+			},
+			'text' : value
+		});
+		if (editable) {
+			var onInput = function(event) {
+				var text = $(event.target).text().trim().replace(/[\t\r\n]/g, ' ');
+				if ($numbers.indexOf(name) == -1) {
+					feature.set(name, text);
+				} else {
+					if (text == '' || isNaN(text)) {
+						feature.unset(name);
+					} else {
+						feature.set(name, Number(text));
+					}
+				}
+			};
+			td.on('input', onInput);
+		}
+		return td;
 	}
 
 	$('body').empty();
@@ -44,19 +83,8 @@ $(document).ready(function() {
 			}).appendTo(tbody);
 			var exitList = $hulop.editor.findExit(feature);
 			$names.forEach(function(name, col) {
-				var text = feature.get(name) || '';
-				if (name == 'floors') {
-					text = floors.map(floorText);
-				}
-				var editable = col > 1;
-				tr.append($('<td>', {
-					'rowspan' : exitList.length || 1,
-					'contenteditable' : editable,
-					'css' : {
-						'background-color' : editable ? '#fff' : '#eee'
-					},
-					'text' : text
-				}));
+				var value = name == 'floors' ? floors.map(floorText) : feature.get(name);
+				tr.append(createCell(feature, name, value, col > 1, exitList.length));
 			});
 			exitList.forEach(function(exit, row) {
 				if (row > 0) {
@@ -72,15 +100,8 @@ $(document).ready(function() {
 					$hulop.indoor.showFloor(floor);
 				});
 				$exit_names.forEach(function(name, col) {
-					var text = name == '高さ' ? floorText(floor) : exit.get(name) || '';
-					var editable = col > 1;
-					tr.append($('<td>', {
-						'contenteditable' : editable,
-						'css' : {
-							'background-color' : editable ? '#fff' : '#eee'
-						},
-						'text' : text
-					}));
+					var value = name == '高さ' ? floorText(floor) : exit.get(name);
+					tr.append(createCell(exit, name, value, col > 1));
 				});
 			});
 		}
