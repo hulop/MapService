3255418ad3ffac2815c5ac6cb7d9e50e54367ccd
Takashi Itoh <jl03313@jp.ibm.com>
2018-09-11 18:29:54 +0900

    Initial DataTables support on facility names editing

--- MapService/WebContent/js/facil_name_editor.js
+++ MapService/WebContent/js/facil_name_editor.js
@@ -1,5 +1,29 @@
-var $names = [ 'facil_id', 'floors', 'hulop_building', 'hulop_major_category', 'hulop_sub_category', 'hulop_show_labels_zoomlevel' ];
-var $numbers = ['hulop_show_labels_zoomlevel'];
+/*******************************************************************************
+ * Copyright (c) 2014, 2017 IBM Corporation, Carnegie Mellon University and
+ * others
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ******************************************************************************/
+var $hulop = window.opener.$hulop;
+var $names = [ 'facil_id', 'floors', 'entrances', 'hulop_building', 'hulop_major_category', 'hulop_sub_category',
+		'hulop_show_labels_zoomlevel' ];
+var $numbers = [ 'hulop_show_labels_zoomlevel' ];
 $names = $names.concat([ 'name_ja', 'name_hira', 'name_en', 'name_es', 'name_fr', 'name_ko', 'name_zh-CN' ]);
 $names = $names.concat([ 'hulop_long_description', 'hulop_long_description_ja', 'hulop_long_description_hira',
 		'hulop_long_description_en', 'hulop_long_description_es', 'hulop_long_description_fr', 'hulop_long_description_ko',
@@ -7,12 +31,100 @@ $names = $names.concat([ 'hulop_long_description', 'hulop_long_description_ja', 
 var $exit_names = [ 'ent#_node', 'ent#_fl', 'ent#_n', 'ent#_n_ja', 'ent#_n_hira', 'ent#_n_en', 'ent#_n_es', 'ent#_n_fr',
 		'ent#_n_ko', 'ent#_n_zh-CN' ];
 
+function isFacility(feature) {
+	return feature.get('facil_id') && feature.get('hulop_major_category') != '_nav_poi_' && feature.get('facil_type') != 10;
+}
+
+function getExitList(feature) {
+	return $hulop.editor.findExit(feature).map(function(exit) {
+		var floor = Number(feature.get('ent' + exit.ent_index + '_fl'));
+		return {
+			'feature' : feature,
+			'floor' : floor,
+			'data' : $exit_names.map(function(name, col) {
+				name = name.replace('#', exit.ent_index);
+				return {
+					'name' : name,
+					'value' : /ent\d_fl/.test(name) ? floorText(floor) : feature.get(name)
+				};
+			})
+		};
+	});
+}
+
+function floorText(floor) {
+	return (floor < 0 ? 'B' + (-floor) : floor) + 'F';
+}
+
 $(document).ready(function() {
-	var $hulop = window.opener.$hulop;
-	function floorText(floor) {
-		return (floor < 0 ? 'B' + (-floor) : floor) + 'F';
+	function getFacilList() {
+		var source = $hulop.map.getRouteLayer().getSource();
+		return source.getFeatures().filter(isFacility).map(function(feature) {
+			var floors = $hulop.editor.getHeights(feature).sort();
+			return {
+				'feature' : feature,
+				'data' : $names.map(function(name, col) {
+					var value;
+					switch (name) {
+					case 'floors':
+						value = floors.map(floorText);
+						break;
+					case 'entrances':
+						value = $hulop.editor.findExit(feature).length;
+						break;
+					default:
+						value = feature.get(name);
+						break;
+					}
+					return {
+						'name' : name,
+						'value' : value
+					};
+				})
+			};
+		});
 	}
-	function createCell(feature, name, value, editable, rowspan) {
+
+	function createFacil() {
+		// Create table
+		$('#facil').empty();
+		var table = $('<table>').appendTo($('#facil'));
+
+		// Create thead
+		var thead = $('<thead>').appendTo(table);
+		var head_tr = $('<tr>').appendTo(thead);
+		$names.forEach(function(name) {
+			head_tr.append($('<th>', {
+				'css' : {
+					'background-color' : 'lightgreen'
+				},
+				'text' : name.replace(/^hulop_/, '')
+			}));
+		});
+		return table;
+	}
+
+	function createExit(target) {
+		// Create table
+		target.empty();
+		var table = $('<table>').appendTo(target);
+
+		// Create thead
+		var thead = $('<thead>').appendTo(table);
+		var head_tr = $('<tr>').appendTo(thead);
+		$exit_names.forEach(function(name) {
+			head_tr.append($('<th>', {
+				'css' : {
+					'background-color' : 'lightblue'
+				},
+				'text' : name
+			}));
+		});
+		return table;
+	}
+
+	// Create tbody
+	function createCell(feature, name, value, editable) {
 		var color = '#eee';
 		if (editable) {
 			color = '#fff';
@@ -26,7 +138,6 @@ $(document).ready(function() {
 			}
 		}
 		var td = $('<td>', {
-			'rowspan' : rowspan || 1,
 			'contenteditable' : editable,
 			'css' : {
 				'background-color' : color
@@ -51,60 +162,42 @@ $(document).ready(function() {
 		return td;
 	}
 
-	$('body').empty();
-	var table = $('<table>').appendTo($('body'));
-	var thead = $('<thead>').appendTo(table);
+	var table = createFacil();
 	var tbody = $('<tbody>').appendTo(table);
-	var head_tr = $('<tr>').appendTo(thead);
-	$names.forEach(function(name) {
-		head_tr.append($('<th>', {
-			'css' : {
-				'background-color' : 'lightgreen'
-			},
-			'text' : name.replace(/^hulop_/, '')
-		}));
-	});
-	$exit_names.forEach(function(name) {
-		head_tr.append($('<th>', {
-			'css' : {
-				'background-color' : 'lightblue'
-			},
-			'text' : name
-		}));
-	});
-
-	var source = $hulop.map.getRouteLayer().getSource();
-	source.getFeatures().forEach(function(feature) {
-		if (feature.get('facil_id') && feature.get('hulop_major_category') != '_nav_poi_' && feature.get('facil_type') != 10) {
-			var floors = $hulop.editor.getHeights(feature).sort();
-			tr = $('<tr>', {
-				'click' : function() {
-					$hulop.editor.showProperty(feature);
+	var current_facil;
+	getFacilList().forEach(function(facil) {
+		var body_tr = $('<tr>', {
+			'click' : function(event) {
+				if (current_facil != facil) {
+					current_facil = facil;
+					$hulop.editor.showProperty(facil.feature);
+					var exitList = getExitList(facil.feature);
+					if (exitList.length == 0) {
+						$('#exit').empty();
+						return;
+					}
+					var table = createExit($('#exit'));
+					var tbody = $('<tbody>').appendTo(table);
+					exitList.forEach(function(exit, index) {
+						var body_tr = $('<tr>', {
+							'click' : function() {
+								$hulop.indoor.showFloor(exit.floor);
+							}
+						}).appendTo(tbody);
+						exit.data.forEach(function(item, col) {
+							body_tr.append(createCell(exit.feature, item.name, item.value, col > 1));
+						});
+					});
+					table.DataTable();
 				}
-			}).appendTo(tbody);
-			var exitList = $hulop.editor.findExit(feature);
-			$names.forEach(function(name, col) {
-				var value = name == 'floors' ? floors.map(floorText) : feature.get(name);
-				tr.append(createCell(feature, name, value, col > 1, exitList.length));
-			});
-			exitList.forEach(function(exit, row) {
-				if (row > 0) {
-					tr = $('<tr>', {
-						'click' : function() {
-							$hulop.editor.showProperty(feature);
-						}
-					}).appendTo(tbody);
+				if (event.target.cellIndex == 2) {
+					// $(document).scrollTop($('#exit').offset().top);
 				}
-				var floor = Number(feature.get('ent' + exit.ent_index + '_fl'));
-				tr.click(function() {
-					$hulop.indoor.showFloor(floor);
-				});
-				$exit_names.forEach(function(name, col) {
-					name = name.replace('#', exit.ent_index);
-					var value = /ent\d_fl/.test(name) ? floorText(floor) : feature.get(name);
-					tr.append(createCell(feature, name, value, col > 1));
-				});
-			});
-		}
+			}
+		}).appendTo(tbody);
+		facil.data.forEach(function(item, col) {
+			body_tr.append(createCell(facil.feature, item.name, item.value, col > 2));
+		});
 	});
+	table.DataTable()
 });
