5691616dae34eda8bef5ccd7447bc2a55a4f005d
Takashi Itoh <jl03313@jp.ibm.com>
2018-10-19 15:43:17 +0900

    Add translateAll(deltaX, deltaY) command line function

--- MapService/WebContent/js/editor.js
+++ MapService/WebContent/js/editor.js
@@ -1348,7 +1348,27 @@ $hulop.editor = function() {
 		return heights;
 	}
 
+	var ignoreChange = false;
+	window.translateAll = function(deltaX, deltaY) {
+		var count = 0;
+	    ignoreChange = true;
+		try {
+		    $hulop.map.getRouteLayer().getSource().getFeatures().forEach(function(feature) {
+		        feature.getGeometry().translate(deltaX, deltaY);
+		        count++;
+		    });
+		} finally {
+		    ignoreChange = false;
+		}
+		return count;
+	};
+
 	function geometryChanged(feature) {
+		if (ignoreChange) {
+			syncLatlng(feature);
+			setModified(feature);
+			return;
+		}
 		var nodeID = feature.get('node_id');
 		if (nodeID) {
 			var nodeCoordinate = feature.getGeometry().getCoordinates();
